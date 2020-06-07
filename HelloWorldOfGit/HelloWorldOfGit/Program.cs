b25f98a93b35eb53e6eaf71ea8ef33bef474cfde
JJMGsoftworks <jjmgsoftworks@gmail.com>
2020-06-07 20:30:33 +0100

    master conflict

--- HelloWorldOfGit/HelloWorldOfGit/Program.cs
+++ HelloWorldOfGit/HelloWorldOfGit/Program.cs
@@ -8,6 +8,7 @@ namespace HelloWorldOfGit
         {
             Console.WriteLine("Hello World");
             Console.WriteLine("For a pull request");
+            Console.WriteLine("conflict with new feature");
             Console.ReadKey();
         }
     }
