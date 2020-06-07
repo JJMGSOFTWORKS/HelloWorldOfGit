5ec06c5df15509de1b44ce12966192ede46afcbf
JJMGsoftworks <jjmgsoftworks@gmail.com>
2020-06-07 18:43:14 +0100

    added hello world

--- HelloWorldOfGit/HelloWorldOfGit/Program.cs
+++ HelloWorldOfGit/HelloWorldOfGit/Program.cs
@@ -6,7 +6,9 @@ namespace HelloWorldOfGit
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World");
             Console.WriteLine("Hello World!");
+            Console.ReadKey();
         }
     }
 }
